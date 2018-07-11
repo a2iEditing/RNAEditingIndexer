30029e11d8839d69a37711e9101f81882a394433
shalomhillelroth <shalomhillelroth@gmail.com>
2018-07-11 15:52:46 +0300

    p5

--- configure.sh
+++ configure.sh
@@ -6,13 +6,6 @@ BEDTOOLS_PATH="bedtools"
 SAMTOOLS_PATH="samtools"
 RESOURCES_DIR="${DEV_ROOT}/Resources"
 
-HELP_MSG="Optional Params:
-    -h\--help   print this message
-    -j=\--java_home=   set java home dir. (default is: /usr)
-    -b=\--bedtools=   set bedtools invoke command. (default is: bedtools)
-    -s=\--samtools=   set samtools invoke command. (default is: samtools)
-    -r=\--resources_dir=   set the path of the resources dir to download to. (default is: ${RESOURCES_DIR})
-"
 
 for i in "$@"
     do
@@ -46,8 +39,14 @@ esac
 
 case $i in
     -h=*|--help*)
-    echo ${HELP_MSG}
-    exit
+    echo "Optional Params:
+    -h\--help   print this message
+    -j=\--java_home=   set java home dir. (default is: /usr)
+    -b=\--bedtools=   set bedtools invoke command. (default is: bedtools)
+    -s=\--samtools=   set samtools invoke command. (default is: samtools)
+    -r=\--resources_dir=   set the path of the resources dir to download to. (default is: ${RESOURCES_DIR})
+"
+    break
 esac
 
 done
