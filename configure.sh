b27f83f621a229c527a4ff9c065b6ecd68aa8af0
shalomhillelroth <ShalomhillelRoth@gmail.com>
2019-10-30 09:31:28 +0200

    added support for shells with missing bash_source

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-:'
+'
 This work is licensed under the Creative Commons Attribution-Non-Commercial-ShareAlike 4.0 International License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 For use of the software by commercial entities, please inquire with Tel Aviv University at ramot@ramot.org.
-© 2019 Tel Aviv University (Erez Y. Levanon, Erez.Levanon@biu.ac.il;
+(c) 2019 Tel Aviv University (Erez Y. Levanon, Erez.Levanon@biu.ac.il;
 Eli Eisenberg, elieis@post.tau.ac.il;
 Shalom Hillel Roth, shalomhillel.roth@live.biu.ac.il).
 '
@@ -11,6 +11,10 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 DEV_ROOT="$(dirname $(readlink -f ${BASH_SOURCE}))"
+if [ -z "$DEV_ROOT" ]
+then
+DEV_ROOT="$(dirname $0)"
+fi
 JAVA_HOME="/usr"
 BEDTOOLS_PATH="bedtools"
 BAM_UTILS_PATH="bam"
