774acdb12008e08df96eaaa8ed7f6526f71036b6
shalomhillelroth <17741453+shalomhillelroth@users.noreply.github.com>
2019-11-19 19:29:31 +0200

    fixed typo

--- make/initResources.sh
+++ make/initResources.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-:'
+'
 This work is licensed under the Creative Commons Attribution-Non-Commercial-ShareAlike 4.0 International License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 For use of the software by commercial entities, please inquire with Tel Aviv University at ramot@ramot.org.
